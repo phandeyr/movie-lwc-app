af85deef3c7069c285ecc485d48cd0f09ffc0e9b
phandeyr <phandeyr@gmail.com>
2021-03-07 16:35:12 +0000

    amended search service to return only movies and modified movie detail layout

--- src/client/modules/data/movieSearchService/movieSearchService.js
+++ src/client/modules/data/movieSearchService/movieSearchService.js
@@ -4,7 +4,7 @@ import {MOVIE_API_KEY, MOVIE_URL } from '../config.js'
  * @description Fetches api response and returns the data on success
  * @param {*} data the search term object
  */
-export const getMovies = (data) => fetch(`${MOVIE_URL}?s=${data.searchTerm}&r=json&apikey=${MOVIE_API_KEY}`)
+export const getMovies = (data) => fetch(`${MOVIE_URL}?s=${data.searchTerm}&type=movie&r=json&apikey=${MOVIE_API_KEY}`)
     .then((response) => {
         if (!response.ok) throw new Error('An unexpected error occurred')
         return response.json()
